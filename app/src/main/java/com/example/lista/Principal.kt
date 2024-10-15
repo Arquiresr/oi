8e1bed0d3fb529abee9fe5abee2aa9d35d94019a
Arquiresr <81758554+Arquiresr@users.noreply.github.com>
2024-10-14 22:50:02 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/lista/Principal.kt
@@ -0,0 +1,22 @@
+package com.example.lista
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Principal : AppCompatActivity() {
+    private lateinit var btn_lista: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_principal)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
